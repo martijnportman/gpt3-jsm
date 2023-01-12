5e2593e80c5d58e166c90a8d6b13dcb08363d497
Martijn Portman <martijnportman@outlook.com>
2023-01-12 19:36:39 +0100

    Adding assets and logo to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import logo from '../../assets/logo.svg'
 import './navbar.css';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+
+
+//BEM -> Block Element Modifier
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt__navbar-links_logo'>
+          <img src={logo} alt='logo'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
