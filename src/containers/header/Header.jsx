42be3037b3cb506cba7990d6825d06d2c78d8286
Portman, Martijn <mportman@sligro.nl>
2023-01-13 16:52:53 +0100

    adding header..

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,9 +3,16 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="gpt3__header section__padding" id= "home">
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 OpenAI</h1>
+
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your email address'/>
+          <button type='button'>Get Started</button>
+        </div>
       </div>
+    </div>
   )
 }
 
